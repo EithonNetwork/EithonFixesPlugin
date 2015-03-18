0d58b95a10ddbf574aa3a07a4a5298355f753af2
vanbrayne <lars.lindberg@xlent.se>
2015-03-18 18:41:33 +0100

    Made stable command call.

--- src/main/java/se/fredsfursten/eithonfixesplugin/Fixes.java
+++ src/main/java/se/fredsfursten/eithonfixesplugin/Fixes.java
@@ -7,14 +7,14 @@ import se.fredsfursten.plugintools.Misc;
 
 public class Fixes {
 	private static Fixes singleton = null;
-	private static String buyCommand;
-	private static String takeCommand;
+	private static String giveCommandFormat;
+	private static String takeCommandFormat;
 
 	private JavaPlugin plugin = null;
 
 	private Fixes() {
-		buyCommand = EithonFixesPlugin.getPluginConfig().getString("BuyCommand");
-		takeCommand = EithonFixesPlugin.getPluginConfig().getString("TakeCommand");
+		giveCommandFormat = EithonFixesPlugin.getPluginConfig().getString("GiveCommand");
+		takeCommandFormat = EithonFixesPlugin.getPluginConfig().getString("TakeCommand");
 	}
 
 	static Fixes get()
@@ -34,8 +34,26 @@ public class Fixes {
 
 	public void buy(Player buyingPlayer, String item, double price, int amount)
 	{
-		String command = String.format(takeCommand, buyingPlayer.getName(), price*amount);
-		command = String.format(buyCommand, buyingPlayer.getName(), item, amount);
-		Misc.executeCommand(command);
+		String takeCommand;
+		try {
+			takeCommand = String.format(takeCommandFormat, buyingPlayer.getName(), price*amount);
+		} catch (Exception e) {
+			buyingPlayer.sendMessage(String.format(
+					"Could not buy, because the TakeCommand (%s) from config.yml is not correctly formatted.",
+					takeCommandFormat));
+			return;			
+		}
+		
+		String giveCommand;
+		try {
+			giveCommand = String.format(giveCommandFormat, buyingPlayer.getName(), item, amount);
+		} catch (Exception e) {
+			buyingPlayer.sendMessage(String.format(
+					"Could not buy, because the GiveCommand (%s) from config.yml is not correctly formatted.",
+					giveCommandFormat));
+			return;
+		}
+		Misc.executeCommand(takeCommand);
+		Misc.executeCommand(giveCommand);
 	}
 }
